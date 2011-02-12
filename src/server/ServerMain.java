421320eeb722d87bae2bbb27f410836ba1b8a457
Fredrik Stål <dt08fs5@student.lth.se>
2011-02-12 20:57:27 +0100

    lagt till en "test"-keystore och ändrat namn på klasser

---
+++ src/server/ServerMain.java
@@ -0,0 +1,24 @@
+package server;
+
+import java.net.ServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import java.security.cert.X509Certificate;
+
+public class ServerMain {
+	public static void main(String[] args) throws Exception{
+		System.setProperty("javax.net.ssl.keyStore", "keystore");
+	    System.setProperty("javax.net.ssl.keyStorePassword", "eit060");
+	    
+	    SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+	    ServerSocket server = ssf.createServerSocket(7778);
+	    while (true) {
+	      SSLSocket socket = (SSLSocket) server.accept();
+	      SSLSession session = ((SSLSocket) socket).getSession();
+	      X509Certificate cert = (X509Certificate) session.getPeerCertificates()[0];
+	      
+	      server.close();
+	    }
+	}
+}
