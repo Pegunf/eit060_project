2ee49266a7375fd1bba01eabbdb2531222ea6282
Fredrik Stål <fredrik.stal@live.se>
2011-02-24 10:55:19 +0100

    lolol red code

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,21 +1,20 @@
 package server;
 
 import java.net.ServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.SSLSocket;
+import java.net.Socket;
+
+import javax.net.ssl.*;
 import java.security.cert.X509Certificate;
 
 public class ServerMain {
 	public static void main(String[] args) throws Exception{
-		System.setProperty("javax.net.ssl.keyStore", "keystore");
+		System.setProperty("javax.net.ssl.keyStore", "client.keystore.jks");
 	    System.setProperty("javax.net.ssl.keyStorePassword", "eit060");
-	    //lite comments här lolol
-	    //TEST COMMENT
+	    
 	    SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-	    ServerSocket server = ssf.createServerSocket(7778);
+	    ServerSocket server = ssf.createServerSocket(5432);
 	    while (true) {
-	      SSLSocket socket = (SSLSocket) server.accept();
+	      Socket socket = server.accept();
 	      SSLSession session = ((SSLSocket) socket).getSession();
 	      X509Certificate cert = (X509Certificate) session.getPeerCertificates()[0];
 	      
